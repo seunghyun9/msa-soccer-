8b7d61c6e51f374f010a8acfc88fc3b041593408
seunghyun <suji1541@gmail.com>
2022-03-29 17:58:49 +0900

    2022 03 29 DB 연결

---
+++ soccer-express/app/routes/user.routes.js
@@ -0,0 +1,26 @@
+/**
+ 
+
+var express = require('express');
+var router = express.Router();
+
+/* GET users listing. 
+router.get('/', function(req, res, next) {
+  res.send('respond with a resource');
+});
+
+module.exports = router;
+*/
+module.exports = function(app){
+  app,use((req,res,next)=>{
+    res.hearder(
+
+    )
+  });
+  //next();
+  app.get("", ()=>{})
+  app.post("", ()=>{})
+  app.update("", ()=>{})
+  app.delets("", ()=>{})
+
+};
