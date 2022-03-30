bc504ddc474ed9d2de390f519e61ac7ed93450bf
seunghyun <suji1541@gmail.com>
2022-03-30 17:48:56 +0900

    2002 03 30 17:48 수정

--- soccer-express/app.js
+++ soccer-express/app.js
@@ -4,27 +4,22 @@ const express = require('express');
 const mongoose = require('mongoose');
 const app = express();
 const { port, MONGO_URI } = process.env;
-
 app.use(express.static('public'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 app.use(cors());
-
 var corsOptions = {
   origin: 'http://localhost:3000',
   optionsSucessStaus: 200
 }
-app.get('/', (req, res) => {
-  res.json({"현재 시간 : ":new Date().toLocaleString()})
-})
-app.get('/api/now', cors(corsOptions),(req, res) => {
-  res.json({"now":new Date().toLocaleString()})
-})
-
 mongoose
   .connect(MONGO_URI, { useNewUrlParser: true, useUnifiedTopology: true })
   .then(() => console.log('Successfully connected to mongodb'))
   .catch(e => console.error(e));
+app.listen(port, () => {
+  console.log({"현재 시간 : ":new Date().toLocaleString()})
+})
+
 
 app.get('/', (req, res) => {
   res.json({"현재 시간 : ":new Date().toLocaleString()})
@@ -32,7 +27,10 @@ app.get('/', (req, res) => {
 app.get('/api/now', cors(corsOptions),(req, res) => {
   res.json({"now":new Date().toLocaleString()})
 })
-
-app.listen(port, () => {
-  console.log({"현재 시간 : ":new Date().toLocaleString()})
+app.get('/', (req, res) => {
+  res.json({"현재 시간 : ":new Date().toLocaleString()})
 })
+app.get('/api/now', cors(corsOptions),(req, res) => {
+  res.json({"now":new Date().toLocaleString()})
+})
+
